6f81a21c232309d56d730c9471a4a70dab7edd91
vlad <vladkalyuzhnyu@gmail.com>
2020-04-28 13:51:52 +0300

    gradle

--- android/src/pro/superkostya/AndroidLauncher.java
+++ android/src/pro/superkostya/AndroidLauncher.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import pro.superkostya.SuperKostyaPro;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
+
+    @Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
