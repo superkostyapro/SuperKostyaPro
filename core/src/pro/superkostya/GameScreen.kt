47a052614742e29ce964470261c70aa9a2d42257
vlad <vladkalyuzhnyu@gmail.com>
2020-05-01 07:39:48 +0300

    tut

--- core/src/pro/superkostya/GameScreen.kt
+++ core/src/pro/superkostya/GameScreen.kt
@@ -1,19 +1,24 @@
 package pro.superkostya
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
 
-class GameScreen : Screen {
+class GameScreen : Screen, InputProcessor {
 
     private val world = World()
 
     private val renderer = WorldRenderer(world)
 
+    private val controller = WorldController(world)
+
     override fun show() {
+        Gdx.input.inputProcessor = this
     }
 
     override fun resize(width: Int, height: Int) {
+        renderer.resize(width, height)
     }
 
     override fun resume() {
@@ -22,10 +27,44 @@ class GameScreen : Screen {
     override fun render(delta: Float) {
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        controller.update(delta)
         renderer.render()
     }
 
+    override fun keyUp(keycode: Int): Boolean {
+        return false
+    }
+
+    override fun keyDown(keycode: Int): Boolean {
+        return false
+    }
+
+    override fun keyTyped(character: Char): Boolean {
+        return false
+    }
+
+    override fun mouseMoved(screenX: Int, screenY: Int): Boolean {
+        return false
+    }
+
+    override fun scrolled(amount: Int): Boolean {
+        return false
+    }
+
+    override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        return false
+    }
+
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        return false
+    }
+
+    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+        return false
+    }
+
     override fun hide() {
+        Gdx.input.inputProcessor = null
     }
 
     override fun pause() {
