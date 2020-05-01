90ed1d8f0e4d89f3a7f8bea68c7cfe1737c82799
vlad <vladkalyuzhnyu@gmail.com>
2020-05-01 13:19:40 +0300

    fix

--- core/src/pro/superkostya/SuperKostyaPro.kt
+++ core/src/pro/superkostya/SuperKostyaPro.kt
@@ -1,10 +1,22 @@
 package pro.superkostya
 
 import com.badlogic.gdx.Game
+import pro.superkostya.screen.MainScreen
 
 class SuperKostyaPro : Game() {
 
     override fun create() {
-        setScreen(GameScreen())
+        setScreen(MainScreen())
+    }
+
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
+        ppuX = width / CAMERA_WIDTH
+        ppuY = height / CAMERA_HEIGHT
+    }
+
+    override fun dispose() {
+        super.dispose()
+        AssetsManager.getInstance().dispose()
     }
 }
