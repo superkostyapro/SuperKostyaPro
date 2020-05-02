e021681c970f7adf031a70f1fd9c514d33658f41
vlad <vladkalyuzhnyu@gmail.com>
2020-05-02 19:52:52 +0300

    types

--- src/phaser/types/Sound.kt
+++ src/phaser/types/Sound.kt
@@ -1,19 +1,9 @@
 package Phaser.Types.Sound
 
-import kotlin.js.*
-import kotlin.js.Json
-import org.khronos.webgl.*
-import org.w3c.dom.*
-import org.w3c.dom.events.*
-import org.w3c.dom.parsing.*
-import org.w3c.dom.svg.*
-import org.w3c.dom.url.*
-import org.w3c.fetch.*
-import org.w3c.files.*
-import org.w3c.notifications.*
-import org.w3c.performance.*
-import org.w3c.workers.*
-import org.w3c.xhr.*
+import Phaser.Sound.BaseSound
+import Phaser.Sound.BaseSoundManager
+
+typealias EachActiveSoundCallback = (manager: BaseSoundManager, sound: BaseSound, index: Number, sounds: Array<BaseSound>) -> Unit
 
 external interface AudioSpriteSound {
     var spritemap: Any?
