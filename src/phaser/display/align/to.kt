64fce380b7aac60455919a342479d029a4068ffa
vlad <vladkalyuzhnyu@gmail.com>
2020-05-02 16:03:15 +0300

    phaser display align

---
+++ src/phaser/display/align/to.kt
@@ -0,0 +1,11 @@
+package phaser.display.align
+
+import Phaser.GameObjects.GameObject
+
+external fun <G : GameObject> LeftBottom(gameObject: G, alignTo: GameObject, offsetX: Number = definedExternally, offsetY: Number = definedExternally): G
+
+external fun <G : GameObject> LeftTop(gameObject: G, alignTo: GameObject, offsetX: Number = definedExternally, offsetY: Number = definedExternally): G
+
+external fun <G : GameObject> RightBottom(gameObject: G, alignTo: GameObject, offsetX: Number = definedExternally, offsetY: Number = definedExternally): G
+
+external fun <G : GameObject> RightTop(gameObject: G, alignTo: GameObject, offsetX: Number = definedExternally, offsetY: Number = definedExternally): G
