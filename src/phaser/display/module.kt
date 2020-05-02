efbc8cc7b1a8cdaace070ad2d0d2253c53566f0b
vlad <vladkalyuzhnyu@gmail.com>
2020-05-02 15:45:00 +0300

    phaser display

--- src/phaser/display/module.kt
+++ src/phaser/display/module.kt
@@ -1,27 +1,11 @@
-@file:JsQualifier("Phaser.Display")
-@file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION")
-package Phaser.Display
+package phaser.display
 
-import kotlin.js.*
-import kotlin.js.Json
-import org.khronos.webgl.*
-import org.w3c.dom.*
-import org.w3c.dom.events.*
-import org.w3c.dom.parsing.*
-import org.w3c.dom.svg.*
-import org.w3c.dom.url.*
-import org.w3c.fetch.*
-import org.w3c.files.*
-import org.w3c.notifications.*
-import org.w3c.performance.*
-import org.w3c.workers.*
-import org.w3c.xhr.*
-import integer
-import Phaser.Types.Display.InputColorObject
 import Phaser.Types.Display.ColorObject
 import Phaser.Types.Display.HSVColorObject
+import Phaser.Types.Display.InputColorObject
+import integer
 
-external open class Color(red: integer = definedExternally, green: integer = definedExternally, blue: integer = definedExternally, alpha: integer = definedExternally) {
+open external class Color(red: integer = definedExternally, green: integer = definedExternally, blue: integer = definedExternally, alpha: integer = definedExternally) {
     open var gl: Array<Number>
     open fun transparent(): Phaser.Display.Color
     open fun setTo(red: integer, green: integer, blue: integer, alpha: integer = definedExternally, updateHSV: Boolean = definedExternally): Phaser.Display.Color
@@ -79,7 +63,7 @@ external open class Color(red: integer = definedExternally, green: integer = def
     }
 }
 
-external open class BaseShader(key: String, fragmentSrc: String = definedExternally, vertexSrc: String = definedExternally, uniforms: Any = definedExternally) {
+open external class BaseShader(key: String, fragmentSrc: String = definedExternally, vertexSrc: String = definedExternally, uniforms: Any = definedExternally) {
     open var key: String
     open var fragmentSrc: String
     open var vertexSrc: String
