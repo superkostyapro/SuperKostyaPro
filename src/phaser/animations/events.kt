a81b3148d7bf7fb1b80825ba2756ee8fef741e8f
vlad <vladkalyuzhnyu@gmail.com>
2020-05-02 16:22:26 +0300

    phaser animations webgl

--- src/phaser/animations/events.kt
+++ src/phaser/animations/events.kt
@@ -1,21 +1,4 @@
-@file:JsQualifier("Phaser.Animations.Events")
-@file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION")
-package Phaser.Animations.Events
-
-import kotlin.js.*
-import kotlin.js.Json
-import org.khronos.webgl.*
-import org.w3c.dom.*
-import org.w3c.dom.events.*
-import org.w3c.dom.parsing.*
-import org.w3c.dom.svg.*
-import org.w3c.dom.url.*
-import org.w3c.fetch.*
-import org.w3c.files.*
-import org.w3c.notifications.*
-import org.w3c.performance.*
-import org.w3c.workers.*
-import org.w3c.xhr.*
+package phaser.animations
 
 external var ADD_ANIMATION: Any
 
