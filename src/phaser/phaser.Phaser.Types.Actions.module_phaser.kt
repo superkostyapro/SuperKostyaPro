028dcccb498e3d82d9f2b8c9a228876d5fb38fcc
vlad <vladkalyuzhnyu@gmail.com>
2020-05-02 18:40:16 +0300

    phaser redo

---
+++ src/phaser/phaser.Phaser.Types.Actions.module_phaser.kt
@@ -0,0 +1,43 @@
+@file:JsQualifier("Phaser.Types.Actions")
+@file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION")
+package Phaser.Types.Actions
+
+import kotlin.js.*
+import kotlin.js.Json
+import org.khronos.webgl.*
+import org.w3c.dom.*
+import org.w3c.dom.events.*
+import org.w3c.dom.parsing.*
+import org.w3c.dom.svg.*
+import org.w3c.dom.url.*
+import org.w3c.fetch.*
+import org.w3c.files.*
+import org.w3c.notifications.*
+import org.w3c.performance.*
+import org.w3c.workers.*
+import org.w3c.xhr.*
+import integer
+
+external interface GridAlignConfig {
+    var width: integer?
+        get() = definedExternally
+        set(value) = definedExternally
+    var height: integer?
+        get() = definedExternally
+        set(value) = definedExternally
+    var cellWidth: integer?
+        get() = definedExternally
+        set(value) = definedExternally
+    var cellHeight: integer?
+        get() = definedExternally
+        set(value) = definedExternally
+    var position: integer?
+        get() = definedExternally
+        set(value) = definedExternally
+    var x: Number?
+        get() = definedExternally
+        set(value) = definedExternally
+    var y: Number?
+        get() = definedExternally
+        set(value) = definedExternally
+}
