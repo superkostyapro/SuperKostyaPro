a17800c9b7510c3ac20ae3bec9eb60c223e6c1b3
vlad <vladkalyuzhnyu@gmail.com>
2020-05-03 11:30:05 +0300

    optimize

--- src/Phaser/Types/Create/Create.kt
+++ src/Phaser/Types/Create/Create.kt
@@ -1,19 +1,7 @@
 package Phaser.Types.Create
 
-import kotlin.js.*
-import kotlin.js.Json
-import org.khronos.webgl.*
-import org.w3c.dom.*
-import org.w3c.dom.events.*
-import org.w3c.dom.parsing.*
-import org.w3c.dom.svg.*
-import org.w3c.dom.url.*
-import org.w3c.fetch.*
-import org.w3c.files.*
-import org.w3c.notifications.*
-import org.w3c.performance.*
-import org.w3c.workers.*
-import org.w3c.xhr.*
+import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLCanvasElement
 
 typealias GenerateTextureCallback = (canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) -> Unit
 
