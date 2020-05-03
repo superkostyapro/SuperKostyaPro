1624671026395ec72351da92cfca2ff72a867ed0
vlad <vladkalyuzhnyu@gmail.com>
2020-05-03 11:06:45 +0300

    optimize

--- src/Phaser/Math/Math.kt
+++ src/Phaser/Math/Math.kt
@@ -1,23 +1,11 @@
 package Phaser.Math
 
 import kotlin.js.*
-import kotlin.js.Json
 import org.khronos.webgl.*
-import org.w3c.dom.*
-import org.w3c.dom.events.*
-import org.w3c.dom.parsing.*
-import org.w3c.dom.svg.*
-import org.w3c.dom.url.*
-import org.w3c.fetch.*
-import org.w3c.files.*
-import org.w3c.notifications.*
-import org.w3c.performance.*
-import org.w3c.workers.*
-import org.w3c.xhr.*
-import integer
 import Phaser.Geom.Point
 import Phaser.Types.Math.Vector2Like
 import Phaser.Types.Math.SinCosTable
+import Phaser.integer
 
 external fun Average(values: Array<Number>): Number
 
