f65e1f8d55a07be16c8ea863a681888427de9c0f
vlad <vladkalyuzhnyu@gmail.com>
2020-05-03 09:54:09 +0300

    process

--- process.js
+++ process.js
@@ -1,20 +1,32 @@
 const fs = require('fs');
 const path = require('path');
+const process = require('process');
+
+String.prototype.replaceBetween = function(start, end, what) {
+  return this.substring(0, start) + what + this.substring(end);
+};
+
+process.chdir('src/Phaser');
 
 fs.readdir('.', (err, files) => {
   files.forEach(file => {
     if (file.startsWith('phaser')) {
-      let dir = path.dirname(file)
-      const parts = file.split('.')
-      for (let i = 2; i < parts.length - 1; i++) {
+      const data = fs.readFileSync(file, 'utf8');
+      const start = data.indexOf("package ");
+      const end = data.indexOf('\n', start);
+      const pckg = data.substring(start, end);
+      const result = data.replaceBetween(start, end, pckg.substring(0, pckg.lastIndexOf('.')));
+      fs.writeFileSync(file, result, 'utf8');
+      let dir = path.dirname(file);
+      const parts = file.split('.');
+      for (let i = 2; i < parts.length - 2; i++) {
         const part = parts[i];
-        dir = `${dir.toLowerCase()}/${part}`
         if (parts[i + 1] == 'module_phaser') {
-          fs.copyFile(file, `${dir}.kt`, () => {});
-          break;
+          fs.copyFileSync(file, `${dir}/${part}.kt`);
         } else {
-          if (!fs.existsSync(dir.toLowerCase())) {
-            fs.mkdirSync(dir.toLowerCase());
+          dir = `${dir}/${part}`;
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
           }
         }
       }
--- process.sh
+++ process.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+cd src/Phaser
 for file in *.kt; do
-  sed -i '/file:JsQualifier/d' $file
   sed -i '/file:Suppress/d' $file
+  sed -i -- 's/^external open class/open external class/g' *.kt
 done
