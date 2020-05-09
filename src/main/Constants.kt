98b0bbe9ad8a5833f2697d2b33a8eed334aca89e
vlad <vladkalyuzhnyu@gmail.com>
2020-05-09 10:22:27 +0300

    main block

--- src/main/Constants.kt
+++ src/main/Constants.kt
@@ -3,5 +3,4 @@ package main
 const val MIN_LEVEL = 1
 const val MAX_LEVEL = 4
 
-const val UNIT_W = 100f
-const val UNIT_H = 100f
+const val UNIT = 100f
---
+++ src/main/actor/Block.kt
@@ -0,0 +1,31 @@
+package main.actor
+
+import Phaser.GameObjects.Graphics
+import main.UNIT
+import kotlin.math.max
+import kotlin.math.min
+
+fun Graphics.drawCutRect(
+    cX: Float,
+    cY: Float,
+    width: Float = UNIT,
+    height: Float = UNIT
+) {
+    val x = cX - width / 2
+    val y = cY - height / 2
+    fillRect(x, y, width, height)
+    val right = x + width
+    val space = 10f
+    var s = space
+    while (s < height) {
+        val x2 = x + s
+        lineBetween(x, y + s, min(x2, right), y + max(0f, x2 - right))
+        s += space
+    }
+    s -= height
+    while (s < width) {
+        val x2 = x + height + s
+        lineBetween(x + s, y + height, min(x2, right), y + max(0f, x2 - right))
+        s += space
+    }
+}
