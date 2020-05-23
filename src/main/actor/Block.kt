4d784ac3e713c81a2a7030a161736fc17f78e397
vlad <vladkalyuzhnyu@gmail.com>
2020-05-23 13:09:10 +0300

    module fix

--- src/main/actor/Block.kt
+++ src/main/actor/Block.kt
@@ -2,19 +2,19 @@ package main.actor
 
 import Phaser.GameObjects.Graphics
 import main.UNIT
+import main.maze.Cell
+import main.maze.OrthogonalCell
 import kotlin.math.max
 import kotlin.math.min
 
-fun Graphics.drawCut(
-    cX: Float,
-    cY: Float,
-    width: Float = UNIT,
-    height: Float = UNIT
-) {
+fun Graphics.drawCut(cX: Float, cY: Float, vararg sides: Cell.Side) {
+    val width = UNIT
+    val height = UNIT
     val x = cX - width / 2
     val y = cY - height / 2
     fillRect(x, y, width, height)
     val right = x + width
+    val bottom = y + height
     val space = 10f
     var s = space
     while (s < height) {
@@ -25,7 +25,19 @@ fun Graphics.drawCut(
     s -= height
     while (s < width) {
         val x2 = x + height + s
-        lineBetween(x + s, y + height, min(x2, right), y + max(0f, x2 - right))
+        lineBetween(x + s, bottom, min(x2, right), y + max(0f, x2 - right))
         s += space
+    }
+    if (sides.contains(OrthogonalCell.Side.NORTH)) {
+        lineBetween(x, y, right, y)
+    }
+    if (sides.contains(OrthogonalCell.Side.WEST)) {
+        lineBetween(x, y, x, bottom)
+    }
+    if (sides.contains(OrthogonalCell.Side.SOUTH)) {
+        lineBetween(x, bottom, right, bottom)
+    }
+    if (sides.contains(OrthogonalCell.Side.EAST)) {
+        lineBetween(right, y, right, bottom)
     }
 }
