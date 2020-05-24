164cb4de49c155ab2556103f90ea5639819e3aaf
vlad <vladkalyuzhnyu@gmail.com>
2020-05-24 10:58:27 +0300

    main

--- src/main/scene/GameScene.kt
+++ src/main/scene/GameScene.kt
@@ -50,12 +50,11 @@ abstract class GameScene(config: SettingsConfig) : BaseScene(config) {
             val right = cell.getCellOnSide(EAST)
             val left = cell.getCellOnSide(WEST)
             val bottom = cell.getCellOnSide(SOUTH)
-            val sides = mutableListOf<Cell.Side>()
             if (cell.hasSide(NORTH)) {
                 val exists = top?.hasSide(SOUTH) ?: false
                 if (!exists) {
                     (1 until SCALE_X).forEach {
-                        createBlock(x + UNIT * it, y, NORTH, SOUTH)
+                        createBlock(x + UNIT * it, y, listOf(NORTH, SOUTH))
                     }
                 }
             }
@@ -63,62 +62,64 @@ abstract class GameScene(config: SettingsConfig) : BaseScene(config) {
                 val exists = left?.hasSide(EAST) ?: false
                 if (!exists) {
                     (1 until SCALE_Y).forEach {
-                        createBlock(x, y + UNIT * it, WEST, EAST)
+                        createBlock(x, y + UNIT * it, listOf(WEST, EAST))
                     }
                 }
             }
             if (cell.hasSide(EAST)) {
                 (1 until SCALE_Y).forEach {
-                    createBlock(x + UNIT * SCALE_X, y + UNIT * it, WEST, EAST)
+                    createBlock(x + UNIT * SCALE_X, y + UNIT * it, listOf(WEST, EAST))
                 }
             }
             if (cell.hasSide(SOUTH)) {
                 (1 until SCALE_X).forEach {
-                    createBlock(x + UNIT * it, y + UNIT * SCALE_Y, NORTH, SOUTH)
+                    createBlock(x + UNIT * it, y + UNIT * SCALE_Y, listOf(NORTH, SOUTH))
                 }
             }
             if (cell.hasSide(NORTH) || cell.hasSide(WEST)) {
+                val sides = mutableListOf(NORTH, WEST)
                 val topLeft = cell.getCellOnPoint(NORTH_WEST)
                 if (
                     top?.hasSide(SOUTH, WEST) != true &&
                     topLeft?.hasSide(SOUTH, EAST) != true &&
                     left?.hasSide(NORTH, EAST) != true
                 ) {
-                    createBlock(x, y, NORTH, WEST)
+                    createBlock(x, y, sides)
                 }
             }
             if (cell.hasSide(NORTH) || cell.hasSide(EAST)) {
+                val sides = mutableListOf(NORTH)
                 val topRight = cell.getCellOnPoint(NORTH_EAST)
                 if (top?.hasSide(SOUTH, EAST) != true) {
-                    var east: Cell.Side? = null
                     if (right?.hasSide(NORTH) != true && topRight?.hasSide(SOUTH) != true) {
-                        east = EAST
+                        sides.add(EAST)
                     }
-                    createBlock(x + UNIT * SCALE_X, y, NORTH, east)
+                    createBlock(x + UNIT * SCALE_X, y, sides)
                 }
             }
             if (cell.hasSide(SOUTH) || cell.hasSide(WEST)) {
+                val sides = mutableListOf(WEST)
                 val bottomLeft = cell.getCellOnPoint(SOUTH_WEST)
                 if (
                     left?.hasSide(SOUTH, EAST) != true &&
                     bottomLeft?.hasSide(NORTH, EAST) != true
                 ) {
-                    var south: Cell.Side? = null
                     if (bottom?.hasSide(WEST) != true) {
-                        south = SOUTH
+                        sides.add(SOUTH)
                     }
-                    createBlock(x, y + UNIT * SCALE_Y, WEST, south)
+                    createBlock(x, y + UNIT * SCALE_Y, sides)
                 }
             }
             if (cell.hasSide(SOUTH) || cell.hasSide(EAST)) {
+                val sides = mutableListOf<Cell.Side>()
                 val bottomRight = cell.getCellOnPoint(SOUTH_EAST)
-                if (cell.hasSide(NORTH) || top?.hasSide(SOUTH) != true) {
+                if (!cell.hasSide(EAST) && right?.hasSide(WEST) != true) {
                     sides.add(NORTH)
                 }
                 if (right?.hasSide(SOUTH) != true && bottomRight?.hasSide(NORTH) != true) {
                     sides.add(EAST)
                 }
-                if (cell.hasSide(NORTH) || top?.hasSide(SOUTH) != true) {
+                if (!cell.hasSide(SOUTH) && bottom?.hasSide(NORTH) != true) {
                     sides.add(WEST)
                 }
                 if (bottom?.hasSide(EAST) != true && bottomRight?.hasSide(WEST) != true) {
